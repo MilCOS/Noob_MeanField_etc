82fb6e0bc698f142a0e8c2a78c6255ff71f1ac3c
MilCOS <han.xu@whu.edu.cn>
2018-04-23 21:43:16 +0800

    Remove old directory

--- jupyter_Mean_field_approch/.ipynb_checkpoints/import_file-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import pickle
-import json
-from main import mean_field as mf
--- jupyter_Mean_field_approch/test.py
+++
@@ -1,9 +0,0 @@
-from import_file import *
-
-Row=20; Col=Row
-g_sbd_l = [1.0,2.0,3.0,4.0]
-g_tcur_l = [0.0]#[1.0,2.0,3.0,4.0]
-
-tau = 0.00001
-
-mf.main_cluster(Row, Col, tau, g_sbd_l, g_tcur_l)
